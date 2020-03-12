2ffa825675470ccae73b381bd1d3cb79b68ab736
pedrosaints <48567256+pedrosaints@users.noreply.github.com>
2020-03-11 21:22:32 -0300

    Update BufferManager.c

--- BufferManager.c
+++ BufferManager.c
@@ -48,61 +48,60 @@ int BuscaList(struct no *p, int x)
 		return 0;
 		
     }
-    else
+    else 
+	{
         printf("Lista vazia.");
+		return 0;
+	}
 }
 
 //---
-
-/*
-struct no* InitListHeat()
+//1: insercao concluida 0: não deu certo
+//so funciona pq eu ja sei que a lista nao esta cheia
+//não insere sempre no head, verificar isso
+int InsertList(struct no *p,int x)
 {
-	printf("Inicializando a lista heat...\n"); 
-	struct no *head = NULL;
-    struct no *tail = NULL;
-	struct no *aux = NULL;
-	int i;
-	head = (struct no*) malloc(sizeof(struct no*));
-	head->Data = 0;
-	head->ptr = NULL;
-	aux = head;
-	for(i=1; i<(5*buffersize/8); i++)
-	{	
-		tail = (struct no*) malloc(sizeof(struct no*));
-		tail->Data = 0;
-		tail->ptr = NULL;
-		aux->ptr = tail;
-		aux = tail;
-		tail= NULL;
+	int aux1;
+	int aux2;
+	if(p)
+    {
+        if(p->Data==-1)
+		{
+			p->Data=x;
+			return 1;
+		}
+		else
+		{
+			aux1 = p->Data;
+			p->Data=x;
+			p=p->ptr;
+			while(p)
+			{
+			aux2 = p->Data;
+			p->Data = aux1;
+			p=p->ptr;
+			aux1 = aux2;
+			}
+		}
+		return 1;
+		
+    }
+    else 
+	{
+        printf("Lista vazia.");
+		return 0;
 	}
-	return head;
 }
 
 //---
-
-struct no* InitListOld()
+//1: cheia, 0: não cheia
+int ListIsNotFull(struct no *tail)
 {
-	printf("Inicializando a lista old...\n"); 
-	struct no *head = NULL;
-    struct no *tail = NULL;
-	struct no *aux = NULL;
-	int i;
-	head = (struct no*) malloc(sizeof(struct no*));
-	head->Data = 0;
-	head->ptr = NULL;
-	aux = head;
-	for(i=1; i<(3*buffersize/8); i++)
-	{	
-		tail = (struct no*) malloc(sizeof(struct no*));
-		tail->Data = 0;
-		tail->ptr = NULL;
-		aux->ptr = tail;
-		aux = tail;
-		tail= NULL;
-	}
-	return head;
+	if(tail->Data == -1)
+		return 1;
+	else
+		return 0;
 }
-*/
 
 //---
 
@@ -116,13 +115,13 @@ int main () {
 	struct no *auxh = NULL;
 	int i;
 	headh = (struct no*) malloc(sizeof(struct no*));
-	headh->Data = 0;
+	headh->Data = -1;
 	headh->ptr = NULL;
 	auxh = headh;
 	for(i=1; i<(5*buffersize/8); i++)
 	{	
 		tailh = (struct no*) malloc(sizeof(struct no*));
-		tailh->Data = 0;
+		tailh->Data = -1;
 		tailh->ptr = NULL;
 		auxh->ptr = tailh;
 		auxh = tailh;
@@ -137,32 +136,112 @@ int main () {
     struct no *tailo = NULL;
 	struct no *auxo = NULL;
 	heado = (struct no*) malloc(sizeof(struct no*));
-	heado->Data = 0;
+	heado->Data = -1;
 	heado->ptr = NULL;
 	auxo = heado;
 	for(i=1; i<(3*buffersize/8); i++)
 	{	
 		tailo = (struct no*) malloc(sizeof(struct no*));
-		tailo->Data = 0;
+		tailo->Data = -1;
 		tailo->ptr = NULL;
 		auxo->ptr = tailo;
 		auxo = tailo;
 		tailo= NULL;
 	}
 	tailo = auxo;
+	
 	//----
-	
+	//TESTES
 	/*
-	struct no *headh = NULL;
-	struct no *heado = NULL;
-	headh = InitListHeat();
-	heado = InitListOld();
-	*/
+	i=0;
+	while(ListIsNotFull(tailo))
+	{
+		InsertList(heado,i);
+		i++;
+		ShowList(heado);
+	}
 	
-	printf("1: %d\n",BuscaList(heado,1));
-	printf("0: %d\n",BuscaList(heado,0));
+	if(BuscaList(heado,5))
+		printf("5: %d\n",BuscaList(heado,5));
+	else
+		printf("0: %d\n",BuscaList(heado,0));
+	*/
+	//-----------------
+	//MANAGER LRU-MYsql
+	
+		/*O QUE EU TO FAZENDO:
+		GERANDO ALEATORIOS DE 1 A 10;
+		SE EU TIRO 1 (METAFORICAMENTE EU PRECISO DA PAGINA 1)
+		TO ANOTANDO EM UM VETOR CONTADOR, QUANTIDADE QUE CADA NUMERO SAIU
+		[duvida 1]TEORICAMENTE OS NUMEROS QUE MAIS SAIRAM EM SUA MAIORIA DEVEM TA EM HEAT
+		*/
+	// variavel aleatoria
+	int a;
+	//inicialianzndo contador
+	int contador[101];
+	for(i=0;i<101;i++)
+	{
+		contador[i]=0;
+	}
+
+	for(i=0;i<1000;i++)
+	{
+		a = rand() % 100;
+		//printf("%d\n",a);
+		contador[a] = contador[a]+1;
+		if(BuscaList(heado,a))
+		{	
+			//[duvida 2]
+			if (!(BuscaList(headh,a)))
+			{
+				if(ListIsNotFull(tailh))
+					InsertList(headh,a);
+				else
+				{
+					tailh->Data = -1;
+					InsertList(headh,a);
+				}
+			}
+		}
+		else
+			{
+				if(ListIsNotFull(tailo))
+					InsertList(heado,a);
+				else 
+				{
+					tailo->Data = -1;
+					InsertList(heado,a);
+				}
+			}
+		
+		/*
+		ShowList(headh);	
+		ShowList(heado);
+		printf("\n");
+		*/
+	}
+	
+	//-----------------
 	
 	ShowList(headh);	
 	ShowList(heado);	
+	
+	printf("\n[");
+	for(i=0;i<101;i++)
+	{
+		printf(" %d=%d ",i,contador[i]);
+	}
+	printf("]\n");
 	return 0;
 }
+
+
+
+/*
+DUVIDAS
+
+1 - Devo levar em conta o numero de vezes que foi requisitado?
+
+2 - Se eu passo um valor pro heat, eu tiro ele do old?
+
+*/
